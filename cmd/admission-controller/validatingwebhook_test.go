e1e5de78c1a28857b2edabd109c00eb38dbad6bd
cezarybednarz <cezary.bednarz@gmail.com>
2021-03-09 14:26:23 +0100

    first test for Handle()

--- cmd/admission-controller/validatingwebhook_test.go
+++ cmd/admission-controller/validatingwebhook_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/scylladb/go-log"
@@ -11,8 +13,14 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	authenticationv1 "k8s.io/api/authentication/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 func TestValidateClusterChanges(t *testing.T) {
@@ -142,4 +150,96 @@ func TestValidateClusterChanges(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestHandle(t *testing.T) {
+	ctx := log.WithNewTraceID(context.Background())
+	atom := zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	logger, _ := log.NewProduction(log.Config{Level: atom})
+
+	basicCluster := unit.NewDoubleRackCluster("test-cluster", "test-cluster-ns", "repo", "2.3.1", "test-dc",
+		unit.RackInfo{
+			Name:     "rack-1",
+			Members:  3,
+			Capacity: "5Gi",
+			CPU:      "1",
+			Memory:   "500M",
+		},
+		unit.RackInfo{
+			Name:     "rack-2",
+			Members:  2,
+			Capacity: "3Gi",
+			CPU:      "0.5",
+			Memory:   "200M",
+		},
+	)
+
+	offUpdateMode := v1alpha1.UpdateModeOff
+	modeOffSca := v1alpha1.ScyllaClusterAutoscaler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-sca",
+			Namespace: "test-cluster-ns",
+		},
+		Spec: v1alpha1.ScyllaClusterAutoscalerSpec{
+			TargetRef: &v1alpha1.TargetRef{
+				Name:      "test-cluster",
+				Namespace: "test-cluster-ns",
+			},
+			UpdatePolicy: &v1alpha1.UpdatePolicy{
+				UpdateMode: &offUpdateMode,
+			},
+		},
+		Status: v1alpha1.ScyllaClusterAutoscalerStatus{
+			Recommendations: &v1alpha1.ScyllaClusterRecommendations{
+				DataCenterRecommendations: []v1alpha1.DataCenterRecommendations{
+					{
+						Name: "test-dc",
+						RackRecommendations: []v1alpha1.RackRecommendations{
+							{Name: "rack-1", Members: &v1alpha1.RecommendedRackMembers{Target: 2}},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	c := fake.NewFakeClientWithScheme(scheme, basicCluster, modeOffSca.DeepCopy())
+
+	av := &admissionValidator{Client: nil, logger: logger, scyllaClient: c}
+
+	simpleRequest := admission.Request{
+		AdmissionRequest: admissionv1beta1.AdmissionRequest{
+			UserInfo: authenticationv1.UserInfo{
+				Username: "test-user",
+			},
+			Object:    encodeRaw(t, basicCluster.DeepCopy()),
+			OldObject: encodeRaw(t, basicCluster.DeepCopy()),
+		},
+	}
+
+	tests := []struct {
+		name    string
+		req     admission.Request
+		allowed bool
+	}{
+		{
+			name:    "unchanged cluster",
+			req:     simpleRequest,
+			allowed: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			resp := av.Handle(ctx, test.req)
+			fmt.Printf("%s\n", resp.String())
+		})
+	}
+}
+
+// encodeRaw is a helper to encode some data into a RawExtension.
+func encodeRaw(t *testing.T, input interface{}) runtime.RawExtension {
+	data, err := json.Marshal(input)
+	require.NoError(t, err)
+	return runtime.RawExtension{Raw: data}
 }
